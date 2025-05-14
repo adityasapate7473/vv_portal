80e5f5f5c655aa3993cc6a4aa486567b8dbd8d42
Aditya Sapate <152680003+adityasapate7473@users.noreply.github.com>
2025-05-15 03:29:50 +0530

    Update config.js

--- valuedx_training_app_server/config.js
+++ valuedx_training_app_server/config.js
@@ -1,5 +1,5 @@
 const config = {
     // API_BASE_URL: 'http://10.41.6.15:3001',  
-       API_BASE_URL: 'https://vv-portal.onrender.com', 
+       API_BASE_URL: 'https://vv-portal-1.onrender.com', 
   };
   
